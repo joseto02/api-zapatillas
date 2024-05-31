e1d04501ad95b5af7e35249922e52ebff8502606
joseDeveloper <jom.gonzalez@duocuc.cl>
2024-05-31 14:46:39 -0400

    probando

--- zapatillas.js
+++ zapatillas.js
@@ -1,11 +1,11 @@
 
-const zapatillas = [
+const zapatillasAdidas = [
     {
         "id": 1,
         "nombre": "forum low cl",
         "marca": "adidas",
         "talla": [36, 37, 38, 44],
-        "precio": 89990,
+        "precio": "89990",
         "img": "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/1822e1180d644baca09724689c16721f_9366/Zapatillas_Forum_Low_CL_Blanco_IG3777_01_standard.jpg"
     },
 
@@ -14,7 +14,7 @@ const zapatillas = [
         "nombre": "terrex ax3 gore-tex",
         "marca": "adidas",
         "talla": [38, 39, 40],
-        "precio": 109990,
+        "precio": "109990",
         "img": "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/99452d20a79c48c7b60506e5116449a5_9366/Zapatillas_de_Senderismo_Terrex_AX3_GORE-TEX_Corte_Medio_Negro_IF4876_HM1.jpg"
     },
     {
@@ -22,7 +22,7 @@ const zapatillas = [
         "nombre": "sl 72 rs",
         "marca": "adidas",
         "talla": [38, 39, 40, 41, 42],
-        "precio": 89990,
+        "precio": "89990",
         "img":"https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/faa71d9ca6fa4964b7cf3f68cd117c9f_9366/Zapatillas_SL_72_RS_Granate_JI1280_01_standard.jpg"
     },
     {
@@ -30,7 +30,7 @@ const zapatillas = [
         "nombre": "harden volume 8",
         "marca": "adidas",
         "talla": [38, 39, 40, 41, 42],
-        "precio": 159990,
+        "precio": "159990",
         "img":"https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/7cac1a06dcb34466b2c87fe416709677_9366/Zapatillas_Harden_Volume_8_Blanco_IE2695_01_standard.jpg"
     },
     {
@@ -38,7 +38,7 @@ const zapatillas = [
         "nombre": "air jordan 1 retro high og",
         "marca": "nike",
         "talla": [38, 39, 40,],
-        "precio": 186990,
+        "precio": "186990",
         "img":"https://nikeclprod.vtexassets.com/arquivos/ids/954133-1200-1200?v=638417346119130000&width=1200&height=1200&aspect=true"
     },
     {
@@ -46,7 +46,7 @@ const zapatillas = [
         "nombre": "nike dunk low retro",
         "marca": "nike",
         "talla": [38, 39],
-        "precio": 116990,
+        "precio": "116990",
         "img":"https://nikeclprod.vtexassets.com/arquivos/ids/967061-1200-1200?v=638475981761400000&width=1200&height=1200&aspect=true"
     },
     {
@@ -54,7 +54,7 @@ const zapatillas = [
         "nombre": "nike air force 1 '07'",
         "marca": "nike",
         "talla": [38, 39, 40],
-        "precio": 116990,
+        "precio": "116990",
         "img":"https://nikeclprod.vtexassets.com/arquivos/ids/967907-1200-1200?v=638475995375370000&width=1200&height=1200&aspect=true"
     },
     {
@@ -62,7 +62,7 @@ const zapatillas = [
         "nombre": "rs-x ferrari",
         "marca": "puma",
         "talla": [38, 39, 40, 41, 42, 44],
-        "precio": 109990,
+        "precio": "109990",
         "img":"https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa/global/308064/01/sv01/fnd/CHL/w/1000/h/1000/fmt/png"
     },
     {
@@ -70,7 +70,7 @@ const zapatillas = [
         "nombre": "suede puma x dazed and confused",
         "marca": "puma",
         "talla": [38, 39, 40],
-        "precio": 109990,
+        "precio": "109990",
         "img":"https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa/global/397322/01/sv01/fnd/CHL/w/1000/h/1000/fmt/png"
     },
     {
@@ -78,11 +78,11 @@ const zapatillas = [
         "nombre": "ca pro lux iii",
         "marca": "puma",
         "talla": [38, 39, 40, 41, 42],
-        "precio": 89990,
+        "precio": "89990",
         "img": "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa/global/395203/11/sv01/fnd/CHL/w/1000/h/1000/fmt/png"
     }
 ];
 
 module.exports = {
-    zapatillas
+    zapatillasAdidas
 }
